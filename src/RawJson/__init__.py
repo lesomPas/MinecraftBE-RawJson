d0d5035ff85aed209ecbfcce5510aed300755d15
lesomPas <mcyyds27@163.com>
2025-07-24 14:42:48 +0800

    Update __init__.py

--- src/RawJson/__init__.py
+++ src/RawJson/__init__.py
@@ -1,8 +1,8 @@
 from .main import process
-from .classes import RawText, RawScore, RawSelector, RawTranslate, Rawtext
+from .classes import Text, Score, Selector, Translate, Rawtext
 
-__all__ = ["process", "RawText", "RawScore", "RawSelector", "RawTranslate", "Rawtext"]
-__version__ = "1.0.0"
+__all__ = ["process", "Text", "Score", "Selector", "Translate", "Rawtext"]
+__version__ = "1.0.1"
 __author__ = "lesomras"
 
 Rawtext.process = staticmethod(process)
